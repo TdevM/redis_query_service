c86e0614016168ecf20ce7dc84f292a3560c79d5
tdevm <cr14b17@gmail.com>
2021-02-14 12:05:43 +0530

    add new route

--- src/api/testapis/index.js
+++ src/api/testapis/index.js
@@ -5,6 +5,13 @@ let routes = [
         method: 'GET',
         path: '/hello',
         handler: handlers.hello
+    },
+    {
+        method: 'GET',
+        path: '/server',
+        handler: (request, h) => {
+            return request
+        }
     }
 ];
 
